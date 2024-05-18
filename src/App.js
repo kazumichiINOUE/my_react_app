1b0cee3de4fe0f788e2666e5ad3d2d2acfbdc443
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2024-05-18 17:51:21 +0900

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Section1 from './Section1';
+import exampleImage from './images/dog.png'; // 画像ファイルのパスを指定
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <h1>Hello, World!</h1>
+        <p>This is a web site for my first project.</p>
+        <Section1 />
+        <img src={exampleImage} alt="Example" />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Section1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section2 = () => {
+  return (
+    <div>
+      <section>テキストの作成について</section> 
+      <article>sectionタグ，articleタグを使う例です．</article>
+      <article>画像はimgタグで出しています．</article>
+    </div>
+  );
+};
+
+export default Section2;
